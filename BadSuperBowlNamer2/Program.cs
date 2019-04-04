c48d3f20d84d354d19782f43ea038ba7215c1ec8
Shane D Wilson <me@shanedwilson.com>
2019-04-03 19:30:26 -0500

    Add project files.

---
+++ BadSuperBowlNamer2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BadSuperBowlNamer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
